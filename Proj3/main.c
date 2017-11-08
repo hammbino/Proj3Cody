d37287fd821dd38ecb40f20aa15aa1132cead4e1
Jeffrey Hammond <hammondjeffrey@gmail.com>
2017-11-07 18:57:22 -0700

    Initial Commit

---
+++ Proj3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Proj3
+//
+//  Created by Jeffrey Hammond on 11/7/17.
+//  Copyright © 2017 Jeffrey Hammond. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
